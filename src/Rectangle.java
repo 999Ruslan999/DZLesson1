2e249b20047865744eabd1efaf5305233330d6f7
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-02-02 22:26:31 +0300

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,15 @@
+public class Pryamougolnik {
+
+   public int width;
+
+   public int length;
+
+
+    public static int ploshad(int length, int width) {
+        return length * width;
+    }
+
+    public static int perimetr(int length, int width) {
+        return (length + width) * 2;
+    }
+}
